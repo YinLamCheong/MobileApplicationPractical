8224ab167bd73946af8aa157487a61cd75ed3f54
YinLamCheong <cheongyl-wm18@student.tarc.edu.my>
2021-03-17 18:28:33 +0800

    Week 8 practical

--- RecycleviewExample/app/src/main/java/com/example/recycleviewexample/MainActivity.kt
+++ RecycleviewExample/app/src/main/java/com/example/recycleviewexample/MainActivity.kt
@@ -3,19 +3,19 @@ package com.example.recycleviewexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recycleviewexample.data.StudentDB
 import com.example.recycleviewexample.data.StudentEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
-    //store list of student
-    //val studentList = ArrayList<Student>()
+    //lateinit = assign data later
+   // lateinit var RecList: Array<StudentEntity>
+    lateinit var RecList: Array<StudentEntity>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,11 +29,10 @@ class MainActivity : AppCompatActivity() {
 
             val stdDAO = StudentDB.getDatabase(applicationContext).studentDAO()
 
-
+            //allow statement run in bg
             CoroutineScope(IO).launch{
                 stdDAO.addStudent(aStudent)
             }
-
 
 
             /* //when use list
@@ -46,6 +45,31 @@ class MainActivity : AppCompatActivity() {
             recyclerView.setHasFixedSize(true)
 
              */
+        }
+
+        val btnGet:Button = findViewById(R.id.btnGet)
+        btnGet.setOnClickListener(){
+            //take from db
+            val stdDAO = StudentDB.getDatabase(applicationContext).studentDAO()
+            var studentList = ArrayList<Student>()
+
+            //use main because relate to UI
+            CoroutineScope(Main).launch {
+                RecList = stdDAO.getAll()
+
+
+                for (s in RecList) {
+                    studentList.add(Student(R.drawable.ic_face, s.name, s.programme))
+                }
+
+
+                val recyclerView: RecyclerView = findViewById(R.id.rvStudent)
+
+                //adapt the data into the recycleview
+                recyclerView.adapter = MyAdapter(studentList)
+                recyclerView.layoutManager = LinearLayoutManager(application)
+                recyclerView.setHasFixedSize(true)
+            }
         }
     }
 }
